8a3f59ce860403a9d99d63068a06c874ee42a904
Leonardo Caldas <leonardo.ferreira.caldas@gmail.com>
2019-06-04 20:14:01 +0200

    adding focus and disabled to component

--- src/component.vue
+++ src/component.vue
@@ -1,74 +1,85 @@
 <template lang="html">
-  <input type="tel"
-         :value="formattedValue"
-         @change="change"
-         v-money="{precision, decimal, thousands, prefix, suffix}"
-         class="v-money" />
+    <input type="tel"
+           :value="formattedValue"
+           @change="change"
+           ref="input"
+           :disabled="disabled"
+           v-money="{precision, decimal, thousands, prefix, suffix}"
+           class="v-money"/>
 </template>
 
 <script>
-import money from './directive'
-import defaults from './options'
-import {format, unformat} from './utils'
+    import money from "./directive";
+    import defaults from "./options";
+    import {format, unformat} from "./utils";
 
-export default {
-  name: 'Money',
-  props: {
-    value: {
-      required: true,
-      type: [Number, String],
-      default: 0
-    },
-    masked: {
-      type: Boolean,
-      default: false
-    },
-    precision: {
-      type: Number,
-      default: () => defaults.precision
-    },
-    decimal: {
-      type: String,
-      default: () => defaults.decimal
-    },
-    thousands: {
-      type: String,
-      default: () => defaults.thousands
-    },
-    prefix: {
-      type: String,
-      default: () => defaults.prefix
-    },
-    suffix: {
-      type: String,
-      default: () => defaults.suffix
-    }
-  },
+    export default {
+        name : "Money",
+        props: {
+            value    : {
+                required: true,
+                type    : [Number, String],
+                default : 0
+            },
+            masked   : {
+                type   : Boolean,
+                default: false
+            },
+            precision: {
+                type   : Number,
+                default: () => defaults.precision
+            },
+            decimal  : {
+                type   : String,
+                default: () => defaults.decimal
+            },
+            thousands: {
+                type   : String,
+                default: () => defaults.thousands
+            },
+            prefix   : {
+                type   : String,
+                default: () => defaults.prefix
+            },
+            suffix   : {
+                type   : String,
+                default: () => defaults.suffix
+            },
+            disabled : {
+                type   : Boolean,
+                default: false
+            }
+        },
 
-  directives: {money},
+        directives: {money},
 
-  data () {
-    return {
-      formattedValue: ''
-    }
-  },
+        data() {
+            return {
+                formattedValue: ""
+            };
+        },
 
-  watch: {
-    value: {
-      immediate: true,
-      handler (newValue, oldValue) {
-        var formatted = format(newValue, this.$props)
-        if (formatted !== this.formattedValue) {
-          this.formattedValue = formatted
+        watch: {
+            value: {
+                immediate: true,
+                handler(newValue, oldValue) {
+                    const formatted = format(newValue, this.$props);
+
+                    if (formatted !== this.formattedValue) {
+                        this.formattedValue = formatted;
+                    }
+                }
+            }
+        },
+
+        methods: {
+            change(evt) {
+                this.$emit("input", this.masked ? evt.target.value : unformat(evt.target.value, this.precision));
+            },
+
+            focus() {
+                this.$refs.input.focus();
+            }
         }
-      }
-    }
-  },
-
-  methods: {
-    change (evt) {
-      this.$emit('input', this.masked ? evt.target.value : unformat(evt.target.value, this.precision))
-    }
-  }
-}
+    };
 </script>
